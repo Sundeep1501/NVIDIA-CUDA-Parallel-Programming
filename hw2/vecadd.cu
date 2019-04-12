9f18b171eb9ac23e23e9194e62fa06004b92113d
class account - AUM <aumcls01@dmcvlogin3.asc.edu>
2019-04-12 12:25:50 -0500

    matrix vector

--- hw2/vecadd.cu
+++ hw2/vecadd.cu
@@ -1,119 +1,120 @@
 #include <iostream>
-#include <stdlib.h>
-#include <sstream>
-#include <iomanip>
+
 using namespace std;
 
+#define TILE_WIDTH 64
 #define iceil(num,den) (num+den-1)/den
 
-//Kernel Function
-__global__ void matrixMulKernel(float* d_matrix1, int r1, int c1, float* d_matrix2, int r2, int c2, float *d_matrixR){
-	
-	//Access the pixel on the image
-	int r = blockDim.x * blockIdx.x + threadIdx.x;
-	int c = blockDim.y * blockIdx.y + threadIdx.y;
-
-	//Check if the index is in the image 
-	if(c < c2 && r < r1){
-		
-		int value = 0;
-		for(int i = 0; i < r2; i++){
-			value+= d_matrix1[r*c1+i] * d_matrix2[i*c2+c];
-		}
-		d_matrixR[r*c2 + c] = value;
-	}
-}
-
-void matrixMul(float* matrix1, int r1, int c1, float* matrix2, int r2, int c2, float* matrixR) {
-	
-	//This number of bytes are going be allocated and transferred
-	int size1 = r1 * c1 * sizeof(float);
-	int size2 = r2 * c2 * sizeof(float);
-	int sizeR = r1 * c2 * sizeof(float);
-
-	float *d_matrix1, *d_matrix2, *d_matrixR;
-
-	//Allocate memory from GPU for my input and output array
-	cudaMalloc((void**)&d_matrix1, size1);
-	cudaMalloc((void**)&d_matrix2, size2);
-	cudaMalloc((void**)&d_matrixR, sizeR);
-
-	//Transfer data to the GPU
-	cudaMemcpy(d_matrix1, matrix1, size1, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_matrix2, matrix2, size2, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_matrixR, matrixR, sizeR, cudaMemcpyHostToDevice);
-
-	dim3 myBlockDim(16, 16, 1);
-	dim3 myGridDim(iceil(r1, 16), iceil(c2, 16), 1);
-	//Kernel Launch
-	matrixMulKernel <<<myGridDim, myBlockDim >>> (d_matrix1, r1, c1, d_matrix2, r2, c2, d_matrixR);
-
-	//Transfer results back to CPU
-	cudaMemcpy(matrixR, d_matrixR, sizeR, cudaMemcpyDeviceToHost);
-
-	// free cuda memory
-	cudaFree(d_matrix1);
-	cudaFree(d_matrix2);
-	cudaFree(d_matrixR);
-}
-
-std::string to_format(const int number) {
-    std::stringstream ss;
-    ss << std::setw(2) << std::setfill(' ') << number;
-    return ss.str();
-}
-
 //Prints the image on screen
-void printImage(float* img, int r, int c) {
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			cout << to_format(img[i*c + j]) << " ";
+void printMatrix(float* img, int w, int h) {
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			cout << img[i*w + j] << " ";
 		}
 		cout << endl;
 	}
-	cout << endl;
+	cout <<"*****" << endl;
+}  
+
+__global__ void matrixMulKernel_NoSM(float *d_M, float *d_N, float *d_P, int width){ 
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if(col < 1 && row < width){
+	float pValue = 0;
+        for(int k = 0; k < width; k++) 
+        {
+            pValue += d_M[row*width + k] * d_N[k + col];
+        }
+        d_P[row + col] = pValue;
+    }
 }
 
-int main(int argc, char **argv){
+__global__ void matrixMulKernel_SM(float *d_M, float *d_N, float *d_P, int width){ 
+    
+	__shared__ float Ms[TILE_WIDTH][TILE_WIDTH];
+	__shared__ float Ns[TILE_WIDTH][1];
+	
+	int bx = blockIdx.x; int by = blockIdx.y;
+	int tx = threadIdx.x; int ty = threadIdx.y;
+	
+	int row = by * TILE_WIDTH + ty; 
+    	int col = bx + tx; 
+    
+	float pValue = 0;
+	
+	for(int m=0; m<width/TILE_WIDTH; m++){
+		Ms[ty][tx] = d_M[(m*TILE_WIDTH + tx) + row*width];
+		Ns[ty][tx] = d_N[(m*TILE_WIDTH + ty) + col];
+		__syncthreads();
+		
+        for(int k = 0; k < TILE_WIDTH; k++)
+            pValue += Ms[ty][k] * Ns[k][tx];
 
-	int r1 = atoi(argv[1]);
-	int c1 = atoi(argv[2]);
-	int r2 = atoi(argv[3]);
-	int c2 = atoi(argv[4]);
+		__syncthreads();
+    	}
+	d_P[row + col] = pValue;
+}
 
-	// basic matrix-matrix multiplication rule
-	if (c1 != r2){
-		cout << "#columns of first matrix and #rows of second matrix should be equal.";
-		return 0;
-	}
+void matrixMul(float* M, float* N, float* P, int width) {
+	
+	//This number of bytes are going be allocated and transferred
+	int size = width * width * sizeof(float);
+	int rsize = width * sizeof(float);
 
-	float *matrix1 = new float[r1*c1];
-	float *matrix2 = new float[r2*c2];
-	float *matrixR = new float[r1*c2];
+	float *d_M, *d_N, *d_P; //Device Pointers
 
-	time_t t;
-	srand((unsigned) time(&t));
+	//Allocate memory from GPU for my input and output array
+	cudaMalloc((void**)&d_M, size);
+	cudaMalloc((void**)&d_N, rsize);
+	cudaMalloc((void**)&d_P, rsize);
+
+	//Transfer data to the GPU
+	cudaMemcpy(d_M, M, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_N, N, rsize, cudaMemcpyHostToDevice);
+	
+	dim3 myBlockDim(1, TILE_WIDTH, 1);
+	dim3 myGridDim(iceil(width, TILE_WIDTH), iceil(width, TILE_WIDTH), 1);
+	
+	//===== Not using Shared Memory ===============
+	matrixMulKernel_NoSM <<<myGridDim, myBlockDim >>> (d_M, d_N, d_P, width);
+	cudaMemcpy(P, d_P, rsize, cudaMemcpyDeviceToHost);
+	
+	printMatrix(P, 1, width);
+
+	//===== Using Shared Memory ===================
+	matrixMulKernel_SM <<<myGridDim, myBlockDim >>> (d_M, d_N, d_P, width);
+	cudaMemcpy(P, d_P, size, cudaMemcpyDeviceToHost);
+	
+	printMatrix(P, 1, width);
+	
+	//----------------------------------------------------
+	cudaFree(d_M);
+	cudaFree(d_N);
+	cudaFree(d_P);
+}
+
+int main(){
+
+	srand(time(0));
+
+	int width = 320;
+	float *M = new float[width*width];
+	float *N = new float[width];
+	float *P = new float[width];
 
 	//Load value to the image
-	for (int i = 0; i < r1; i++)
-		for (int j = 0; j < c1; j++)
-			matrix1[i*c1 + j] = rand() % 10 ;
+	for (int i = 0; i < width; i++)
+		for (int j = 0; j < width; j++)
+			M[i*width + j] = rand()%10;
 
-	// Load value to filter
-	for(int i = 0; i < r2; i++)
-		for(int j = 0; j < c2; j++)
-			matrix2[i*c2 + j] = rand() % 10;
-	
-	cout << "Matrix1 Array" << endl;
-	printImage(matrix1, r1, c1);
+	for (int i = 0; i < width; i++)
+		N[i] = rand()%10;
 
-	cout << "Matrix2 Array" << endl;
-	printImage(matrix2, r2, c2);
+	printMatrix(M, width, width);
+	printMatrix(N, 1, width);
 
-	matrixMul(matrix1, r1, c1, matrix2, r2, c2, matrixR);
-	
-	cout << "Result Array" << endl;
-	printImage(matrixR, r1, c2);
+	matrixMul(M, N, P, width);
 
 	return 0;
 }
